0f9ab0ba3a4d5a63131b7c0ca9aeb9f97f328f53
lwy <2447698068@qq.com>
2022-03-30 16:23:51 +0800

    生成函数修饰符代码

--- generators/myGenerate/functionQualifier.js
+++ generators/myGenerate/functionQualifier.js
@@ -1,3 +1,14 @@
 'use strict';
 
 goog.require('Blockly.Solidity');
+
+Blockly.Solidity['function_visibility'] = function (block) {
+    var code = " " + block.getFieldValue('function_visib') + " ";
+    return code;
+}
+
+Blockly.Solidity['function_type'] = function (block) {
+    var code = " " + block.getFieldValue('function_ty') + " ";
+    return code;
+}
+
